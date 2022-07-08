085c8d4554adf6d6a7463c582f77429a9cb8a58b
Vincent Tate <vincentjtate@gmail.com>
2022-07-08 13:12:04 -0700

    small changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button')); // instantiate MDC markup
 
-fetch (`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15
-`)
+fetch (`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15`)
 .then (function (response) {
     console.log(response)
     return response.json();
@@ -10,3 +9,14 @@ fetch (`https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=15
     console.log(data)
 });
 
+
+var slider = document.getElementById('#slider');
+
+noUiSlider.create(slider, {
+    start: [80],
+    connect: true,
+    range: {
+        'min': 0,
+        'max': 100
+    }
+});
